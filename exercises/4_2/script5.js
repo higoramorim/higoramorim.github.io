d4c276db054747ed76eb42794c443ea83dec5657
higoramorim <1810042@aluno.univesp.br>
2020-07-14 19:49:10 -0300

    add files

--- exercises/4_2/script5.js
+++ exercises/4_2/script5.js
@@ -7,25 +7,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     }
 console.log(Array.max(numbers));
 
+let higherNumber = 0;
+for (let i=0; i < numbers.length; i += 1){
+    if(numbers[i] > higherNumber){
+        higherNumber = numbers[i];
+    }
 
-for (i = 0; i < numbers.length; i++){
-    if (numbers[0] > numbers[1]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[2]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[3]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[4]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[5]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[6]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[7]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[8]){
-        console.log(numbers[0]);
-    }else if (numbers[0] > numbers[9]){
-        console.log(numbers[0]);
 }
-}
+console.log("maior num é: "+ higherNumber);
--- exercises/4_2/script6.js
+++ exercises/4_2/script6.js
@@ -4,13 +4,12 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-function exibirEhParEhImpar(numbers){
-   for (let i = 0; i <=numbers.length; i++ ){
-       if (i % 2 === 0){
-           console.log("PAR");
-       }else{
-           console.log("IMPAR");
-       }
-   } 
+let oddNumber = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+    if(numbers[i] % 2 !==0){
+        oddNumber += 1;
+    }
 }
-exibirEhParEhImpar(numbers);
+
+console.log("quant impares: "+ oddNumber);
