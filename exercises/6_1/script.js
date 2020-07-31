13a45ca86843feaed4168903fedf57f9682e03a2
higoramorim <1810042@aluno.univesp.br>
2020-07-31 19:34:47 -0300

    js

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,5 +1,22 @@
 const optionDad = document.querySelector('#option-states');
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+const states = ['AM', 'AC', 'MG', 'RJ', 'SP', 'TO', 'RO', 'RR', 'PA', 'RS', 'PR', 'PA', 'MA', 'PI','GO', 'DF','BA', 'CE', 'MT','MS','ES','SC','AL','AP','PB','RN','SE'];
+const orderedStates = states.sort();
+
+const list = document.getElementById("option-states")
+
+function statesTarget(){
+for (let index = 0; index < orderedStates.length; index++) {
+    element = document.createElement('option');
+    element.innerHTML = orderedStates[index];
+    list.appendChild(element);
+}
+}
+
+
+
+
+
+
+window.onload =  ()=>{
+    statesTarget();
 }
